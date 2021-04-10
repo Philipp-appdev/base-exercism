2afdcf8523b80dcd00febddd09e3cf5637a15bd9
Phil255 <philipp.spitta@chicagobooth.edu>
2021-04-10 21:29:19 +0000

    resistor color duo completed

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -6,3 +6,14 @@ To get started with TDD, see the `README.md` file in your
 `ruby/resistor-color-duo` directory.
 =end
 
+class ResistorColorDuo
+  def self.value(input_array)
+    resistor_values = { "black" => 0, "brown" => 1, "red" => 2, "orange" => 3, "yellow" => 4, "green" => 5, "blue" => 6, "violet" => 7, "grey" => 8, "white" => 9 }
+    output = ""
+    input_array.each do |value|
+      output += resistor_values[value].to_s
+    end
+    output = output[0, 2]
+    output.to_i
+  end
+end
