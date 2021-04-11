983c4986a32b296337d73c5afa6461b72874b9b2
Phil255 <philipp.spitta@chicagobooth.edu>
2021-04-11 15:45:40 +0000

    not saving properly WIP

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -6,3 +6,22 @@ To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
 
+class Matrix(input_string)
+    def initialize
+        @string = input_string
+    end
+
+    def row(n)
+        row = @string.split(/\n/)[n].split.map(&:to_i)
+        row
+    end
+
+    def column(n)
+        array_of_rows = @string.split
+        column = ""
+        array_of_rows.each do |row|
+            column << row[n]
+        end
+        column = column.split.map(&:to_i)
+    end
+end
